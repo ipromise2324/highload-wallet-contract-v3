94c01e3c30f6158914efa101f7a0b7aad1fa2c88
ipromise2324 <168allen55@gmail.com>
2024-06-25 15:51:10 +0800

    feat: send batch until is success

--- scripts/sendBatch.ts
+++ scripts/sendBatch.ts
@@ -19,26 +19,38 @@ export async function run(provider: NetworkProvider) {
     const curQuery = new HighloadQueryId();
     let outMsgs: OutActionSendMsg[] = new Array(254);
 
+    // 0QCg05dcxHO09Ydrw-yTuexzMUa8iJmYAO4eWmyqfgVnDZ_0
     // You can pack your own messages here
     const testBody = beginCell().storeUint(0, 32).storeStringTail('Test highload-wallet-v3').endCell();
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < 1; i++) {
         outMsgs[i] = {
             type: 'sendMsg',
             mode: SendMode.NONE,
             outMsg: internal_relaxed({
                 to: Address.parse('0QAHg-2Oy8Mc2BfENEaBcoDNXvHCu7mc28KkPIks8ZVqwmzg'),
-                value: toNano('0.01'),
+                value: toNano('0.05'),
                 body: testBody,
             }),
         };
     }
 
-    await highloadWalletV3.sendBatch(
-        keyPair.secretKey,
-        outMsgs,
-        SUBWALLET_ID,
-        curQuery,
-        DEFAULT_TIMEOUT,
-        Math.floor(Date.now() / 1000) - 10,
-    );
+    for (let i = 1; i < 100; i++) {
+        console.log(i);
+        try {
+            await highloadWalletV3.sendBatch(
+                keyPair.secretKey,
+                outMsgs,
+                SUBWALLET_ID,
+                curQuery,
+                DEFAULT_TIMEOUT,
+                Math.floor(Date.now() / 1000) - 10,
+            );
+            console.log('Success');
+            break;
+        } catch (e) {
+            console.log(e);
+            // Sleep for 1 second
+            // await new Promise((resolve) => setTimeout(resolve, 300));
+        }
+    }
 }
