a4a8d91d6bc73594ee1f329a41c2f84f3ad1fbdb
ipromise2324 <168allen55@gmail.com>
2024-06-30 22:00:45 +0800

    feat: update highload wallet deployment amount to 1 nano

--- scripts/deployHighloadWallet.ts
+++ scripts/deployHighloadWallet.ts
@@ -22,7 +22,7 @@ export async function run(provider: NetworkProvider) {
         ),
     );
 
-    await highloadWalletV3.sendDeploy(provider.sender(), toNano('0.05'));
+    await highloadWalletV3.sendDeploy(provider.sender(), toNano('1'));
     await provider.waitForDeploy(highloadWalletV3.address, 10, 5);
 
     const chalk = require('chalk');
